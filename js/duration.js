86eb9446cf359e8ab7648531a86cadcdd1bad674
chris2000520 <zhaowuya@foxmail.com>
2020-12-08 21:30:39 +0800

    Site updated: 2020-12-08 21:30:35

--- js/duration.js
+++ js/duration.js
@@ -20,7 +20,7 @@
       if(String(snum).length === 1 ){
         snum = "0" + snum;
       }
-      document.getElementById("timeDate").innerHTML = "本小破站已安全运行&nbsp"+dnum+"&nbsp天";
+      document.getElementById("timeDate").innerHTML = "已安全运行&nbsp"+dnum+"&nbsp天";
       document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
     }
     setInterval(update, 1000);
